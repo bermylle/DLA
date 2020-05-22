7cabf6c0988d88b7e4e5ac3032dba138762e5522
bermylle <bermylle@gmail.com>
2020-05-22 21:12:12 +0800

    add:

--- dlrc/lrc/views.py
+++ dlrc/lrc/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from users.decorators import unauthenticated_user, allowed_users, admin_only
 from django.contrib.auth.decorators import login_required
-@login_required(login_url = 'login')
+
+
 def home(request):
-	return render(request, 'lrc/main.html')
-	
-@login_required(login_url = 'login')
+	return render(request, 'lrc/homepage.html')
+
+
 def about(request):
 	return render(request, 'lrc/about.html')
--- dlrc/users/views.py
+++ dlrc/users/views.py
@@ -70,23 +70,6 @@ def profile(request):
 	}
 	return render(request, 'users/profile.html', context)
 
-# def register(request):
-# 	if request.method == 'POST':
-# 		form = CreateUserForm(request.POST)
-# 		if form.is_valid():
-# 			form.save()
-# 			username = form.cleaned_data.get('username')
-# 			messages.success(request, f'Account created for {username}!')
-
-# 			return redirect('login')
-# 	else:
-# 		form = CreateUserForm()
-
-# 	context = {
-# 		'form' : form
-# 	}
-
-# 	return render(request,'users/register.html', context)
 
 
 
